5a900d9fc69f14792457eaeb2316d3747d0c9e00
minwoo9629 <minwoo9629@gmail.com>
2024-08-19 22:48:02 +0900

    feat : MyPick 구현하기

---
+++ src/pick/index.ts
@@ -0,0 +1,28 @@
+/**
+ * T에서 K 프로퍼티만 선택해 새로운 오브젝트 타입을 만드는 내장 제네릭 Pick<T, K>을 이를 사용하지 않고 구현하기
+ */
+import { Equal, Expect } from "@/utils";
+
+type MyPick<T, K extends keyof T> = { [key in K]: T[key] };
+
+type cases = [
+  Expect<Equal<Expected1, MyPick<Todo, "title">>>,
+  Expect<Equal<Expected2, MyPick<Todo, "title" | "completed">>>,
+  // @ts-expect-error
+  MyPick<Todo, "title" | "completed" | "invalid">,
+];
+
+interface Todo {
+  title: string;
+  description: string;
+  completed: boolean;
+}
+
+interface Expected1 {
+  title: string;
+}
+
+interface Expected2 {
+  title: string;
+  completed: boolean;
+}
