141d56481d6be9bf5efea817f9eac29d2e4ba8f0
민우 <minwoo9629@gmail.com>
2025-01-16 08:42:47 +0900

    feat : prettier, eslint 설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,46 +6,39 @@ module.exports = {
     browser: true,
   },
   extends: [
-    "eslint:recommended",
-    "plugin:prettier/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:import/recommended",
-    "plugin:import/typescript",
-    "plugin:import/errors",
-    "plugin:import/warnings",
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:import/errors',
+    'plugin:import/warnings',
   ],
-  parser: "@typescript-eslint/parser",
-  plugins: ["prettier", "@typescript-eslint", "import"],
+  parser: '@typescript-eslint/parser',
+  plugins: ['prettier', '@typescript-eslint'],
   rules: {
-    "prettier/prettier": ["error", { endOfLine: "auto" }],
-    "import/order": [
-      "error",
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    'import/order': [
+      'error',
       {
-        groups: [
-          "builtin",
-          "external",
-          "internal",
-          "parent",
-          "sibling",
-          "index",
-        ],
-        "newlines-between": "always", // 그룹 간에 줄 바꿈을 항상 추가
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+        'newlines-between': 'always', // 그룹 간에 줄 바꿈을 항상 추가
       },
     ],
-    "import/no-unresolved": "error", // import 시 모듈을 찾을 수 없는 경우 오류로 처리
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-unused-vars": "off",
-    "@typescript-eslint/no-empty-object-type": "off",
-    "@typescript-eslint/ban-ts-comment": "off",
+    'import/no-unresolved': 'error', // import 시 모듈을 찾을 수 없는 경우 오류로 처리
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-empty-object-type': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
   },
   settings: {
-    "import/parsers": {
-      "@typescript-eslint/parser": [".ts", ".tsx"],
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
     },
-    "import/resolver": {
+    'import/resolver': {
       typescript: {
         alwaysTryTypes: true,
-        project: "./tsconfig.json",
+        project: './tsconfig.json',
       },
     },
   },
