dc7780b3527a8c370705277667efeaba04d31bad
민우 <minwoo9629@gmail.com>
2025-01-20 08:39:34 +0900

    feat : Concat 구현하기

---
+++ src/concat/index.ts
@@ -0,0 +1,18 @@
+type ArrayType = readonly unknown[];
+type Concat<T extends ArrayType, U extends ArrayType> = [...T, ...U];
+
+/* _____________ 테스트 케이스 _____________ */
+import type { Equal, Expect } from '@/utils';
+
+const tuple = [1] as const;
+
+type cases = [
+  Expect<Equal<Concat<[], []>, []>>,
+  Expect<Equal<Concat<[], [1]>, [1]>>,
+  Expect<Equal<Concat<typeof tuple, typeof tuple>, [1, 1]>>,
+  Expect<Equal<Concat<[1, 2], [3, 4]>, [1, 2, 3, 4]>>,
+  Expect<Equal<Concat<['1', 2, '3'], [false, boolean, '4']>, ['1', 2, '3', false, boolean, '4']>>,
+];
+
+// @ts-expect-error
+type error = Concat<null, undefined>;
