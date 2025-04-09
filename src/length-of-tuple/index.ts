e7321faff3925212051d0d8cdd9bc35884d246dd
민우 <minwoo9629@gmail.com>
2025-04-10 08:36:49 +0900

    feat : Length of Tuple 구현하기

---
+++ src/length-of-tuple/index.ts
@@ -0,0 +1,17 @@
+import { Equal, Expect } from '@/utils';
+
+type Length<T extends readonly unknown[]> = T['length'];
+
+/* _____________ 테스트 케이스 _____________ */
+
+const tesla = ['tesla', 'model 3', 'model X', 'model Y'] as const;
+const spaceX = ['FALCON 9', 'FALCON HEAVY', 'DRAGON', 'STARSHIP', 'HUMAN SPACEFLIGHT'] as const;
+
+type cases = [
+  Expect<Equal<Length<typeof tesla>, 4>>,
+  Expect<Equal<Length<typeof spaceX>, 5>>,
+  // @ts-expect-error
+  Length<5>,
+  // @ts-expect-error
+  Length<'hello world'>,
+];
