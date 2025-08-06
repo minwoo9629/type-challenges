26d74516180d60472f235a5f8328755ac9287a40
민우 <minwoo9629@gmail.com>
2025-08-07 08:18:39 +0900

    feat : push 구현하기

---
+++ src/push/index.ts
@@ -0,0 +1,17 @@
+type Push<T extends unknown[], U> = [...T, U];
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@/utils';
+
+type cases = [
+  Expect<Equal<Push<[], 1>, [1]>>,
+  Expect<Equal<Push<[1, 2], '3'>, [1, 2, '3']>>,
+  Expect<Equal<Push<['1', 2, '3'], boolean>, ['1', 2, '3', boolean]>>,
+];
+
+type errors = [
+  // @ts-expect-error
+  Expect<Equal<Push<number[], string>, string[]>>,
+  // @ts-expect-error
+  Expect<Equal<Push<string[], number>, [string, number]>>,
+];
